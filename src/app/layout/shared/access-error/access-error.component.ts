5c16f805ffbfbc9511dbba5e612242e50724e9f3
xiurik <irwin.romero.rdz@gmail.com>
2021-10-08 13:00:18 -0500

    update(eslint)

--- src/app/layout/shared/access-error/access-error.component.ts
+++ src/app/layout/shared/access-error/access-error.component.ts
@@ -1,32 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-access-error',
   templateUrl: './access-error.component.html',
   styleUrls: ['./access-error.styles.scss']
 })
-export class AccessErrorComponent implements OnInit {
-  //#region 'Input, Output, ViewChild'
-  //#endregion 'Input, Output, ViewChild'
-
-  //#region 'Variables'
-  //#endregion 'Variables'
-
-  //#region 'Angular Life Cycle'
-  constructor() {}
-
-  ngOnInit(): void {}
-  //#endregion 'Angular Life Cycle'
-
-  //#region 'Load'
-  //#endregion 'Load'
-
-  //#region 'General Methods'
-  //#endregion 'General Methods'
-
-  //#region 'Validations'
-  //#endregion 'Validations'
-
-  //#region 'CRUD'
-  //#endregion 'CRUD'
-}
+export class AccessErrorComponent {}
--- src/app/layout/shared/page-not-found/page-not-found.component.ts
+++ src/app/layout/shared/page-not-found/page-not-found.component.ts
@@ -1,32 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.styles.scss']
 })
-export class PageNotFoundComponent implements OnInit {
-  //#region 'Input, Output, ViewChild'
-  //#endregion 'Input, Output, ViewChild'
-
-  //#region 'Variables'
-  //#endregion 'Variables'
-
-  //#region 'Angular Life Cycle'
-  constructor() {}
-
-  ngOnInit(): void {}
-  //#endregion 'Angular Life Cycle'
-
-  //#region 'Load'
-  //#endregion 'Load'
-
-  //#region 'General Methods'
-  //#endregion 'General Methods'
-
-  //#region 'Validations'
-  //#endregion 'Validations'
-
-  //#region 'CRUD'
-  //#endregion 'CRUD'
-}
+export class PageNotFoundComponent {}
